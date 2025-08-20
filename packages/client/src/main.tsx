2a6aca6b0b2bf4b97a0e88120a2c1113e331691c
Yulia Zhmakina <yazhmakina@sberbank.ru>
2025-08-20 19:12:06 +0300

    feat(LB-22): service worker

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+export function startServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then(reg => console.log('[SW] registered, scope: ', reg.scope))
+        .catch(err => console.log('[SW] registration failed: ', err))
+    });
+  }
+}
+
+startServiceWorker();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
